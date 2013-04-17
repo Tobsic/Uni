5bb8a5e3db81321b5a8ce7a2c6ce3fe53f204545
Tobsic <Tobsic@Tobsic-PC>
2013-04-17 22:35:05 +0200

    Gauß und Recursive Ergebnisse ausgeben

--- Uni/Determinanten/Det.java
+++ Uni/Determinanten/Det.java
@@ -7,19 +7,20 @@ public class Det{
 			for(int span = 0; span < dem; span++)
 				matrix[row][span] = Console.getDouble("Wert f¸r " + (row + 1) + ". Zeile, " + (span + 1) + ". Spalte");
 		Console.writeln();
-		Console.writeln("Die Determinate ist: {0}", det(matrix));
+		Console.writeln("Die Determinante ist laut Rekursion: {0}", detRecursiv(matrix));
+		Console.writeln("Die Determinante ist laut Gauﬂ:      {0}", detByGauﬂ(matrix));
 	}
 	
-	public static double det(double[][] matrix) throws Exception{
+	public static double detRecursiv(double[][] matrix) throws Exception{
 		int dimension = matrix.length;
 		if(dimension < 2)
 			throw new Exception("Die Matrix muss mindestens 2x2 sein.");
 		for(double[] r : matrix)
 			if(r.length != dimension)
 				throw new Exception("Die Matrix muss Quadratisch sein.");
-		return detByGauﬂ(matrix);
+		return _detRecursiv(matrix);
 	}
-	private static double _det(double[][] matrix){
+	private static double _detRecursiv(double[][] matrix){
 		int dimension = matrix.length;
 		if(dimension == 2)
 			return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
@@ -29,11 +30,20 @@ public class Det{
 			for(int copyRow = 0; copyRow < dimension - 1; copyRow++)
 				for(int copySpan = 1; copySpan < dimension; copySpan++)
 					subdeterminant[copyRow][copySpan - 1] = matrix[copyRow >= selectedRow ? copyRow + 1 : copyRow][copySpan];
-			result *= (selectedRow % 2 * -2 + 1) * matrix[selectedRow][0] *  _det(subdeterminant);
+			result *= (selectedRow % 2 * -2 + 1) * matrix[selectedRow][0] *  _detRecursiv(subdeterminant);
 		}
 		return result;
 	}
-	public static double detByGauﬂ(double[][] matrix){
+	public static double detByGauﬂ(double[][] matrix) throws Exception{
+		int dimension = matrix.length;
+		if(dimension < 2)
+			throw new Exception("Die Matrix muss mindestens 2x2 sein.");
+		for(double[] r : matrix)
+			if(r.length != dimension)
+				throw new Exception("Die Matrix muss Quadratisch sein.");
+		return _detByGauﬂ(matrix);
+	}
+	public static double _detByGauﬂ(double[][] matrix){
 		for(int i = 0; i < matrix.length; i++)
 			for(int j = i + 1; j < matrix.length; j++){
 				for(int k = i + 1; k < matrix.length; k++)
