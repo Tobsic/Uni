65403fe4373e08ec726a22c0ac12ce9cd1ef17c1
Tobsic <Tobsic@Tobsic-PC>
2013-04-17 22:25:36 +0200

    Console.getDouble Determinanten (Funktionierend mit Rekursion und Gauß)

---
+++ Uni/Determinanten/Det.java
@@ -0,0 +1,51 @@
+public class Det{
+	public static void main(String[] args) throws Exception{
+		Console.clear();
+		int dem = Console.getInteger("Anzahl der Spalten/Zeilen");
+		double[][] matrix = new double[dem][dem];
+		for(int row = 0; row < dem; row++)
+			for(int span = 0; span < dem; span++)
+				matrix[row][span] = Console.getDouble("Wert f¸r " + (row + 1) + ". Zeile, " + (span + 1) + ". Spalte");
+		Console.writeln();
+		Console.writeln("Die Determinate ist: {0}", det(matrix));
+	}
+	
+	public static double det(double[][] matrix) throws Exception{
+		int dimension = matrix.length;
+		if(dimension < 2)
+			throw new Exception("Die Matrix muss mindestens 2x2 sein.");
+		for(double[] r : matrix)
+			if(r.length != dimension)
+				throw new Exception("Die Matrix muss Quadratisch sein.");
+		return detByGauﬂ(matrix);
+	}
+	private static double _det(double[][] matrix){
+		int dimension = matrix.length;
+		if(dimension == 2)
+			return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+		double[][] subdeterminant = new double[dimension - 1][dimension - 1];
+		double result = 1;
+		for(int selectedRow = 0; selectedRow < dimension; selectedRow++){
+			for(int copyRow = 0; copyRow < dimension - 1; copyRow++)
+				for(int copySpan = 1; copySpan < dimension; copySpan++)
+					subdeterminant[copyRow][copySpan - 1] = matrix[copyRow >= selectedRow ? copyRow + 1 : copyRow][copySpan];
+			result *= (selectedRow % 2 * -2 + 1) * matrix[selectedRow][0] *  _det(subdeterminant);
+		}
+		return result;
+	}
+	public static double detByGauﬂ(double[][] matrix){
+		for(int i = 0; i < matrix.length; i++)
+			for(int j = i + 1; j < matrix.length; j++){
+				for(int k = i + 1; k < matrix.length; k++)
+					if(matrix[i][i] == 0)
+						return 0;
+					else
+						matrix[j][k] -= matrix[i][k] * matrix[j][i] / matrix[i][i];
+				matrix[j][i] = 0;
+			}
+		double result = 1;
+		for(int i = 0; i < matrix.length; i++)
+			result *= matrix[i][i];
+		return result;
+	}
+}
--- Uni/Vector/Console.java
+++ Uni/Vector/Console.java
@@ -10,26 +10,12 @@ import java.text.MessageFormat;
  * @author Tobias Brosge (539713)
  *
  */
-public class Console {
 
+public class Console {
 	private static BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-	public static Float getFloat(String Message) throws IOException{
-		System.out.print(Message + ": ");
-		String value = console.readLine();
-		while(!value.matches("[-]?[0-9]+(\\.[0-9]+)?")){
-			System.out.print("Die Eingabe muss numerisch sein: ");
-			value = console.readLine();
-		}
-		return java.lang.Float.parseFloat(value);
-	}
-	public static int getInteger(String Message) throws IOException {
-		System.out.print(Message + ":");
-		String value = console.readLine();
-		while(!value.matches("[-]?[0-9]+")){
-			System.out.print("Sie müssen eine Ganzzahl eingeben: ");
-			value = console.readLine();
-		}
-		return Integer.parseInt(value);
+	
+	public static void write(String Message, Object... args){
+		System.out.print(MessageFormat.format(Message, args));
 	}
 	public static void writeln(){
 		System.out.println();
@@ -43,5 +29,33 @@ public class Console {
 	public static void clear(){
 		for(int i = 0; i < 20; i++)
 			writeln();
+	}
+	
+	public static float getFloat(String Message) throws IOException{
+		System.out.print(Message + ": ");
+		String value = console.readLine();
+		while(!value.matches("[-]?[0-9]+(\\.[0-9]+)?")){
+			System.out.print("Die Eingabe muss numerisch sein: ");
+			value = console.readLine();
+		}
+		return Float.parseFloat(value);
+	}
+	public static int getInteger(String Message, Object...args) throws IOException {
+		write(Message + ":", args);
+		String value = console.readLine();
+		while(!value.matches("[-]?[0-9]+")){
+			write("Sie müssen eine Ganzzahl eingeben: ");
+			value = console.readLine();
+		}
+		return Integer.parseInt(value);
+	}
+	public static double getDouble(String Message, Object...args) throws IOException{
+		write(Message + ":", args);
+		String value = console.readLine();
+		while(!value.matches("[-]?[0-9]+(.[0-9]+)?")){
+			write("Die Eingabe muss numerisch sein: ");
+			value = console.readLine();
+		}
+		return Double.parseDouble(value);
 	}
 }
